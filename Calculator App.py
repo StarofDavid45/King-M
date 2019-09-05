a9ae1c8a1f7a829ca57079a132378a1a29cd5bff
StarofDavid45 <54926156+StarofDavid45@users.noreply.github.com>
2019-09-05 10:08:25 +0100

    Add files via upload
    Well, this is a calculator app you can use to solve using any operator of your choice like addition, subtraction, multiplication, quotient and remainder etc. You can check out my YouTube channel to see how i did that! It's my first by the way but it's cool!

---
+++ Calculator App.py
@@ -0,0 +1,25 @@
+
+
+Name = input("Enter your full name: ")
+print("You are welcome" + "," + Name)
+variable = input("Enter number of inputs: ")
+if variable >= "3":
+    repr("Enter another number:")
+number1 = float(input("Enter first number: "))
+sign = input("Operator: ")
+number2 = float(input("Enter another number: "))
+if sign == "+":
+    print(number1 + number2)
+elif sign == "-":
+    print(number1 - number2)
+elif sign == "/":
+    print(number1 / number2)
+elif sign == "*":
+    print(number1 * number2)
+elif sign == "**":
+    print(number1 ** number2)
+elif sign == "%":
+    print(number1 % number2)
+else:
+    print(SyntaxError)
+var = exit == input("End program")
